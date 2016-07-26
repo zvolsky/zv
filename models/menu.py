740e7c949ac07cbbee3dbb0bb9b83157beaf74a9
zvolsky <zvolsky@seznam.cz>
2016-07-26 18:10:24 +0200

    title

--- models/menu.py
+++ models/menu.py
@@ -3,5 +3,6 @@
 response.logo = A(B('zítra na výlet'),
                   _class="navbar-brand",_href="http://www.zitranavylet.cz/",
                   _id="znv-logo")
+response.title = "Zítra na výlet"
 
 if "auth" in locals(): auth.wikimenu()
