8fd353a536df89aa217c7bd0c8db4510857ad7f7
Mirek Zvolský <zvolsky@seznam.cz>
2017-07-31 19:16:48 +0200

    individ loc

--- controllers/default.py
+++ controllers/default.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 def index():
-    redirect(URL('portal', 'uvodem'))
+    redirect(URL('portal', 'main'))
 
 
 def user():
--- controllers/portal.py
+++ controllers/portal.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
 
-def uvodem():
-    return {}
+def main():
+    loc = None
+    if auth.user_id:
+        loc = db((db.user_loc.auth_user_id == auth.user_id) & (db.user_loc.weather == True)).select(
+            db.loc.w24, db.loc.yr,
+            join=db.loc.on(db.loc.id == db.user_loc.loc_id)
+        ).first()
+    return dict(loc=loc)
